e923061016eb508c42b2c87a6d4d7fd1e39e5f96
ljlopez <ljlopez@fundacionsafa.es>
2024-09-25 20:11:33 +0200

    Subida - clase Miércoles 23

--- src/test/java/com/sl/safajobs/SafajobsApplicationTests.java
+++ src/test/java/com/sl/safajobs/SafajobsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.sl.safajobs;
 
+import com.sl.safajobs.modelos.Aptitud;
+import com.sl.safajobs.repositorios.AptitudRepositorio;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SafajobsApplicationTests {
 
+    @Autowired
+    private AptitudRepositorio aptitudRepositorio;
+
     @Test
-    void contextLoads() {
+    void testFindAllAptitudes() {
+
+        List<Aptitud> aptituds = aptitudRepositorio.findAll();
+        for(Aptitud a :aptituds){
+            System.out.println(a.getTitulo());
+        }
+
     }
 
 }
